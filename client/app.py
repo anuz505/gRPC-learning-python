f856fe60072b83ebbbd03ebbd42b3d1b1f36d66a
anuz505 <anujb364@gmail.com>
2026-03-27 10:27:04 +0545

    basic gRPC

---
+++ client/app.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import logging
+import os
+import grpc
+import users_pb2
+import users_pb2_grpc
+
+
+def run():
+    target = os.getenv("GRPC_TARGET", "localhost:50051")
+    with grpc.insecure_channel(target) as channel:
+        grpc.channel_ready_future(channel).result(timeout=10)
+        stub = users_pb2_grpc.UserServiceStub(channel)
+        response = stub.SayHelloUser(
+            users_pb2.HelloRequest(
+                username="anuz505",
+                email="anuz505@gmail.com"
+            )
+        )
+        print("Client received: " + response.message)
+
+        all_users_response = stub.GetUsers(users_pb2.GetUsersRequest())
+        print(all_users_response)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
